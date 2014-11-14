bb025ced61425dd29017c797dfa6a1fe77f1519d
TeamFahQ <teamfahq1@gmail.com>
2014-11-14 10:07:48 -0600

    revert root fix

--- device.mk
+++ device.mk
@@ -269,7 +269,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 	persist.sys.usb.config=mtp \
-	ro.secure=0
 
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 $(call inherit-product, hardware/qcom/msm8960/msm8960.mk)
